6d1cf85502bd7dedcaa482485f77793d373953db
silenceshell <hubottle@gmail.com>
2018-01-23 23:18:47 +0800

    add tower of hanoi

--- recursive/tower-of-hanoi.go
+++ recursive/tower-of-hanoi.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+//https://zh.wikipedia.org/zh-hans/%E6%B1%89%E8%AF%BA%E5%A1%94
+func hanoi(h int, from string, via string, to string) {
+	if h == 1 {
+		fmt.Printf("move %d from %s to %s\n", h, from, to)
+		return
+	}
+	hanoi(h-1, from, to, via)
+	fmt.Printf("move %d from %s to %s\n", h, from, to)
+	hanoi(h-1, via, from, to)
+}
+
 func main() {
-	
+	hanoi(3, "A", "B", "C")
 }
